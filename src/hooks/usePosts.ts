7658e4fdc37b6ef631d2e10e1fb53653b8627017
Samuel Eras <sa.eras@gmx.de>
2024-01-31 20:14:32 +0100

    infinite query

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,26 +9,29 @@ interface Post {
 }
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-  const fetchPosts = () =>
+  const fetchPosts = (pageParam: any) =>
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
         params: {
-          _start: (query.page - 1) * query.pageSize,
+          _start: (pageParam - 1) * query.pageSize,
           _limit: query.pageSize,
         },
       })
       .then((res) => res.data);
 
-  return useQuery<Post[], Error>({
+  return useInfiniteQuery<Post[], Error>({
     queryKey: /* userId ? ["users", userId, "posts"] : */ ["posts", query],
-    queryFn: fetchPosts,
+    queryFn: ({ pageParam }: any) => fetchPosts(pageParam),
+    initialPageParam: 1,
     staleTime: 1 * 60 * 1000, //1m
     placeholderData: keepPreviousData,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 };
 
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import usePosts from "../hooks/usePosts";
+import React from "react";
 
 const PostList = () => {
-  /*   const [userId, setUserId] = useState<number>(); */
   const pageSize = 10;
-  const [page, setPage] = useState(1);
-  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
+  const {
+    data: posts,
+    error,
+    isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = usePosts({ pageSize });
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
@@ -25,24 +30,22 @@ const PostList = () => {
         <option value="4">USer 3</option>
       </select> */}
       <ul className="list-group">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {posts?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
       <button
-        disabled={page === 1}
-        className="btn btn-primary my-3"
-        onClick={() => setPage(page - 1)}
-      >
-        Previous
-      </button>
-      <button
+        disabled={isFetchingNextPage}
         className="btn btn-primary my-3 ms-1"
-        onClick={() => setPage(page + 1)}
+        onClick={() => fetchNextPage()}
       >
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load More"}
       </button>
     </>
   );
