2624f41aeabf0882b7364e3c0bc321e3757a1b17
Samuel Eras <sa.eras@gmx.de>
2024-02-01 20:34:19 +0100

    optimistic updates

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,26 +3,42 @@ import { useRef } from "react";
 import { Todo } from "../hooks/useTodos";
 import axios from "axios";
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
 
-  //useMutation<What we receive, what kind of error, what we send>  But the annotation isnt even need here.
-  const addTodo = useMutation<Todo, Error, Todo>({
+  //useMutation<What we receive, what kind of error, what we send, the context is an object to transfer data between callbacks here its the todolist before updating the cache>
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
-    onSuccess: (savedTodo, newTodo) => {
+    onMutate: (newTodo: Todo) => {
       //Invalidating the Cache and therefore forcing to refetch (Doesnt work with json placeholder)
       /* queryClient.invalidateQueries({
         queryKey: ['todos']
       }); */
       //2. Approach: Updating the data in the cache
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
       queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
         ...(todos || []),
-        savedTodo,
+        newTodo,
       ]);
       if (ref.current) ref.current.value = "";
+      return { previousTodos };
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+    onError: (error, newTodo, context) => {
+      if (ref.current) ref.current.value = newTodo.title;
+      if (!context) return;
+      queryClient.setQueryData<Todo[]>(["todos"], () => context.previousTodos);
     },
   });
   const ref = useRef<HTMLInputElement>(null);
@@ -47,9 +63,7 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary" disabled={addTodo.isPending}>
-            {addTodo.isPending ? "Adding..." : "Add"}
-          </button>
+          <button className="btn btn-primary">Add</button>
         </div>
       </form>
       {addTodo.isError && (
