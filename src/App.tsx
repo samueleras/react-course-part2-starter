8eeda2bb4904988994c31ce2af86803332e598ee
Samuel Eras <sa.eras@gmx.de>
2024-02-07 15:16:24 +0100

    implement reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   return (
     <>
-      <TodoForm />
-      <TodoList />
+      <Counter />
     </>
   );
 }
